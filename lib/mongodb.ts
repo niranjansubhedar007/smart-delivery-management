e7248daaad9cc77036e23b13f677949a23cab7ce
niranjan <niranjansubhedar@gmail.com>
2025-03-18 18:24:48 +0530

    mongo url changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,7 +4,7 @@
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/smart-delivery";
 
 if (!MONGODB_URI) {
   throw new Error('❌ Please define the MONGODB_URI environment variable inside .env.local');
